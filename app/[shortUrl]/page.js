7f17e1376793329c7e46c85a01a5732c6b4c36e5
Divya Dabbara <divyadabbara76@gmail.com>
2025-12-27 17:02:23 +0530

    fix: correct short URL redirection in production

--- app/[shortUrl]/page.js
+++ app/[shortUrl]/page.js
@@ -1,23 +1,23 @@
-import {redirect} from 'next/navigation';
-import clientPromise from '@/lib/mongodb';
+export const runtime = "nodejs"
+export const dynamic = "force-dynamic"
 
+import { redirect } from "next/navigation"
+import clientPromise from "@/lib/mongodb"
 
-export default async function Page({params}){
-    const shortUrl = (await params).shortUrl;
+export default async function Page({ params }) {
+  const { shortUrl } = params
 
-    const client = await clientPromise;
-    const db = client.db("bitlinks");
-    const collection = db.collection("url");
+  const client = await clientPromise
+  const db = client.db("bitlinks")
+  const collection = db.collection("url")
 
-    const doc = await collection.findOne({ shortUrl: shortUrl });
-    console.log(doc);
-    if (doc) {
-        // If the short URL exists, redirect to the original URL
-        redirect(doc.url);
-        
-    }else{
-        redirect(`${process.env.NEXT_PUBLIC_HOST}`);
-    }
+  const doc = await collection.findOne({ shortUrl })
 
-    return <div>My Post: {url}</div>
+  if (doc?.url) {
+    // Redirect to original URL
+    redirect(doc.url)
+  }
+
+  // If short URL not found, go to home page
+  redirect("/")
 }
